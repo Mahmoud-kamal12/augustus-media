42638afc682b6a505829024208098852f3e76df2
Mahmoud Kamal <61159931+Mahmoud-kamal12@users.noreply.github.com>
2026-09-05 02:32:20 +0300

    Use inline API route groups

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,35 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FeedController;
+use App\Http\Controllers\FollowController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\PostController;
 use App\Http\Middleware\AuthenticateApiTokenIfPresent;
 use Illuminate\Support\Facades\Route;
 
-require __DIR__.'/auth.php';
+Route::controller(AuthController::class)->group(function (): void {
+    Route::post('/register', 'register');
+    Route::post('/login', 'login');
+});
 
 Route::middleware(AuthenticateApiTokenIfPresent::class)
-    ->group(__DIR__.'/public_api.php');
+    ->group(function (): void {
+        Route::get('/posts/{post}', [PostController::class, 'show']);
+    });
 
 Route::middleware('auth:sanctum')
-    ->group(__DIR__.'/authenticated_api.php');
+    ->group(function (): void {
+        Route::get('/user', [AuthController::class, 'currentUser']);
+        Route::get('/feed', [FeedController::class, 'index']);
+
+        Route::post('/follow/{userToFollow}', [FollowController::class, 'store']);
+        Route::delete('/follow/{userToUnfollow}', [FollowController::class, 'destroy']);
+
+        Route::post('/posts', [PostController::class, 'store']);
+        Route::delete('/posts/{post}', [PostController::class, 'destroy'])
+            ->middleware('can:delete,post');
+
+        Route::post('/posts/{post}/like', [LikeController::class, 'store']);
+        Route::delete('/posts/{post}/like', [LikeController::class, 'destroy']);
+    });
